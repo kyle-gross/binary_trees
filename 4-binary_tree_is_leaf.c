9ad6b65ee16f9281394b1d1bed395704619b9c81
Kyle <kyg555@outlook.com>
2021-10-12 12:30:03 -0700

    Spaces to tabs

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,11 +9,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (!node)
-        return (NULL);
+	if (!node)
+		return (NULL);
 
-    if (!node->right && !node->left)
-        return (1);
-    else
-        return (0);
+	if (!node->right && !node->left)
+		return (1);
+	else
+		return (0);
 }
