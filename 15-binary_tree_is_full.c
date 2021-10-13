c7f2a06811b1731d47551c4a81e654d470b6d6a2
Kyle <kyg555@outlook.com>
2021-10-13 12:03:56 -0700

    Add task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ *
+ * @tree: ptr to root node
+ *
+ * Return: 1 if full, 0 if not full or tree is NULL
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
+}
